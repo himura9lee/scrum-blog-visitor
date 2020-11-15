a895323537d9196225baf0b81699c657aff77178
Ubuntu <1833006@tongji.edu.cn>
2020-11-15 23:01:36 +0800

    fix: github link updated

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -3,5 +3,5 @@ module.exports = [
     {text: '时间线', link: '/timeline/', icon: 'reco-date'},
     {text: '留言板', link: '/views/other/message-board.html', icon: 'reco-suggestion'},
     {text: '关于我', link: '/views/other/about-me.html', icon: 'reco-account'},
-    {text: 'GitHub', link: 'https://github.com/HimuraLee', icon: 'reco-github'}
+    {text: 'GitHub', link: 'https://github.com/himura9lee', icon: 'reco-github'}
 ]
