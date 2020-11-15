599c71205375bb51fa94a52de3b90fdc14b8808b
Ubuntu <1833006@tongji.edu.cn>
2020-11-15 23:25:13 +0800

    fix: fix nav lind

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -1,7 +1,7 @@
 module.exports = [
     {text: '主页', link: '/', icon: 'reco-home'},
     {text: '时间线', link: '/timeline/', icon: 'reco-date'},
-    {text: '留言板', link: '/views/other/message-board.html', icon: 'reco-suggestion'},
-    {text: '关于我', link: '/views/other/about-me.html', icon: 'reco-account'},
+    {text: '留言板', link: '/views/message-board.html', icon: 'reco-suggestion'},
+    {text: '关于我', link: '/views/about-me.html', icon: 'reco-account'},
     {text: 'GitHub', link: 'https://github.com/himura9lee', icon: 'reco-github'}
 ]
